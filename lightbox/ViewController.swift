6b038648c50196e0e2603ed2bba8c8fe26dcc8d2
shiro019 <shiro019@umn.edu>
2024-01-26 17:18:30 -0600

    Initial Commit

---
+++ lightbox/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lightbox
+//
+//  Created by Vaibhav Shirole on 1/26/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
